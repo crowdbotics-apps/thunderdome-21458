21ab0b8de07853813c1f7b466d7cf660759f118e
Crowdbotics <team@crowdbotics.com>
2020-10-13 23:46:50 +0000

    Update models

--- thunderdome/models.py
+++ thunderdome/models.py
@@ -7,6 +7,19 @@ class Prompt(models.Model):
     title = models.CharField(
         max_length=256,
     )
+    content = models.TextField(
+        null=True,
+        blank=True,
+    )
+    wordcount = models.PositiveSmallIntegerField(
+        max_length=256,
+        null=True,
+        blank=True,
+    )
+    closedate = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
 
 
 # Create your models here.
